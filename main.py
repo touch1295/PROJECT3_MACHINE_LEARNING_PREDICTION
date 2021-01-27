7ab92e398518001c9c25e9c4100b21bd7b4af77d
touch1295 <69025171+touch1295@users.noreply.github.com>
2021-01-26 19:58:49 -0500

    web commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/')
+def home():
+    return templates('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
