500a690690701101271615ad8270d6a4ad626ced
touch1295 <69025171+touch1295@users.noreply.github.com>
2021-01-26 21:34:22 -0500

    resolving commit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/')
+@app.route('/home')
 def home():
-    return templates('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
