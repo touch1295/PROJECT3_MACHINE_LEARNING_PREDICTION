925a5081e446c7edba384088f45583c914c1a6d9
touch1295 <69025171+touch1295@users.noreply.github.com>
2021-01-28 00:17:28 -0500

    flask creating commit

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
     
-    
 @app.route('/data_visualizations')
 def data_visualizations():
     return render_template('tableau.html')
     
-    
 @app.route('/heart_disease_prediction')
 def heart_disease_prediction():
-    return render_template('index.html')
-
-
+    return render_template('mlregression.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
